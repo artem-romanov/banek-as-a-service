cccb051b4593f5fb6d34f2ee3f53ae410c312154
Artem Romanov <xenox2048@gmail.com>
2025-07-07 10:44:01 +0300

    Fix logger time format from sec to mins

--- internal/api/server.go
+++ internal/api/server.go
@@ -9,7 +9,7 @@ func InitializeServer() *echo.Echo {
 	server := echo.New()
 	server.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format:           "time: ${time_custom}: method=${method}, uri=${uri}, status=${status}, error=${error}\n",
-		CustomTimeFormat: "2006-01-02 15:05:05.00",
+		CustomTimeFormat: "2006-01-02 15:04:05.00",
 	}))
 	server.HideBanner = true
 	server.HTTPErrorHandler = CreateErrorHandler()
